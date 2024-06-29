470349612b2fa1d3b8bfb912075301483f89cf06
tttrinhtran <trantran0603@gmail.com>
2024-06-30 05:28:57 +0700

    cham

---
+++ angelhack/src/app/component/hover.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+export default function hover() {
+    return(
+        <>
+            <div className="w-full h-30">
+
+            </div>
+        </>
+    ); 
+}
